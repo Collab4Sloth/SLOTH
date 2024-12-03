81b29b23bc3d9f40d8321b4b07627cc6875309ab
clement plumecocq <clement.plumecocq1@gmail.com>
2024-12-03 17:36:57 +0100

    add: mobility to compute diffusion for f = c ln(c)

--- kernel/Utils/PhaseFieldOptions.hpp
+++ kernel/Utils/PhaseFieldOptions.hpp
@@ -347,7 +347,7 @@ enum class HeatCapacity { Constant, Linear };
 ////////////////////////
 //// Diffusion
 ///////////////////////
-enum class Diffusion { Constant, Linear };
+enum class Diffusion { Constant, Linear, Log };
 
 ////////////////////////
 //// Mobility
