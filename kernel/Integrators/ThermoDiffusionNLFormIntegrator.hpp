b923c5e483afdfd878e8e13c12271f25d1841e3d
INTROÏNI Clément ci230846 <clement.introini@cea.fr>
2025-01-16 13:29:26 +0100

    new: generalization of the thermal diffusion integrator

--- kernel/Integrators/ThermoDiffusionNLFormIntegrator.hpp
+++ kernel/Integrators/ThermoDiffusionNLFormIntegrator.hpp
@@ -1,7 +1,7 @@
 /**
  * @file ThermoDiffusionNLFormIntegrator.hpp
  * @author cp273896  (clement.plumecocq@cea.fr)
- * @brief FV for the mass diffusion equation
+ * @brief VF of an inter-diffusion equation
  * @version 0.1
  * @date 2024-06-06
  *
@@ -10,6 +10,7 @@
  */
 #include <algorithm>
 #include <memory>
+#include <string>
 #include <tuple>
 #include <vector>
 
@@ -24,7 +25,7 @@
 #pragma once
 
 /**
- * @brief  Class dedicated to the FV of the mass diffusion equation
+ * @brief  Class dedicated to the VF of an inter-diffusion equation
  *
  * @tparam SCHEME
  * @tparam DIFFU_NAME
@@ -32,27 +33,21 @@
 template <CoefficientDiscretization SCHEME, Diffusion DIFFU_NAME>
 class ThermoDiffusionNLFormIntegrator : public mfem::NonlinearFormIntegrator {
  private:
-  const Parameters diffusion_params_;
-  mfem::real_t coeff_stab;
-  // mfem::ParGridFunction u_old_, mu_;
-  SlothGridFunction u_old_, mu_;
-  std::vector<mfem::ParGridFunction> aux_gf_;
-  mfem::DenseMatrix gradPsi, gradPsiOld;
-  mfem::Vector Psi, gradU, gradUOld, gradmu, elfunOld, elfunmu;
+  std::vector<std::tuple<std::string, double>> inter_diffusion_coeff_;
+  double coeff_stab_;
 
-  template <typename... Args>
-  double diffusion(mfem::ElementTransformation& Tr, const mfem::IntegrationPoint& ip,
-                   const double u, const Parameters& parameters);
-  template <typename... Args>
-  double diffusion_prime(mfem::ElementTransformation& Tr, const mfem::IntegrationPoint& ip,
-                         const double u, const Parameters& parameters);
+  SlothGridFunction u_old_, mu_;
+
+  std::vector<mfem::ParGridFunction> mu_gf_;
+  mfem::DenseMatrix gradPsi;
+  mfem::Vector Psi, gradMu_;
 
  protected:
-  virtual void get_parameters(const Parameters& vectr_param);
+  void get_parameters(const Parameters& vectr_param);
 
  public:
   ThermoDiffusionNLFormIntegrator(const mfem::ParGridFunction& u_old, const Parameters& params,
-                                  const std::vector<mfem::ParGridFunction>& aux_gf);
+                                  const std::vector<mfem::ParGridFunction>& mu_gf);
   ~ThermoDiffusionNLFormIntegrator();
 
   virtual void AssembleElementVector(const mfem::FiniteElement& el, mfem::ElementTransformation& Tr,
@@ -61,68 +56,28 @@ class ThermoDiffusionNLFormIntegrator : public mfem::NonlinearFormIntegrator {
   virtual void AssembleElementGrad(const mfem::FiniteElement& el, mfem::ElementTransformation& Tr,
                                    const mfem::Vector& elfun, mfem::DenseMatrix& elmat);
 
-  void computeMu();
-
   std::unique_ptr<HomogeneousEnergyCoefficient<ThermodynamicsPotentials::LOG>> get_energy(
       mfem::ParGridFunction* gfu, const double diffu);
 };
+
 ////////////////////////////////////////////////////////
 ////////////////////////////////////////////////////////
 ////////////////////////////////////////////////////////
 ////////////////////////////////////////////////////////
 template <CoefficientDiscretization SCHEME, Diffusion DIFFU_NAME>
 void ThermoDiffusionNLFormIntegrator<SCHEME, DIFFU_NAME>::get_parameters(const Parameters& params) {
-}
-/**
- * @brief Return the value of the diffusion coefficient at integration point
- *
- * @tparam SCHEME
- * @tparam DIFFU_NAME
- * @tparam Args
- * @param Tr
- * @param ip
- * @param gfu
- * @param parameters
- * @return double
- */
-template <CoefficientDiscretization SCHEME, Diffusion DIFFU_NAME>
-template <typename... Args>
-double ThermoDiffusionNLFormIntegrator<SCHEME, DIFFU_NAME>::diffusion(
-    mfem::ElementTransformation& Tr, const mfem::IntegrationPoint& ip, const double u,
-    const Parameters& parameters) {
-  if (SCHEME == CoefficientDiscretization::Implicit) {
-    DiffusionCoefficient<0, DIFFU_NAME> diff_coeff(u, parameters);
-    return diff_coeff.Eval(Tr, ip);
-  } else {
-    DiffusionCoefficient<0, DIFFU_NAME> diff_coeff(&this->u_old_, parameters);
-    return diff_coeff.Eval(Tr, ip);
-  }
-}
+  MFEM_VERIFY(
+      this->mu_gf_.size() == (params.get_size() - 1),
+      "ThermoDiffusionNLFormIntegrator requires as many parameters (inter-diffusion "
+      "coefficients) as auxiliary variables in addition of the stabilization coefficient D");
 
-/**
- * @brief Return the first derivative of the diffusion coefficient vs the variable at integration
- * point
- *
- * @tparam SCHEME
- * @tparam DIFFU_NAME
- * @tparam Args
- * @param Tr
- * @param ip
- * @param gfu
- * @param parameters
- * @return double
- */
-template <CoefficientDiscretization SCHEME, Diffusion DIFFU_NAME>
-template <typename... Args>
-double ThermoDiffusionNLFormIntegrator<SCHEME, DIFFU_NAME>::diffusion_prime(
-    mfem::ElementTransformation& Tr, const mfem::IntegrationPoint& ip, const double u,
-    const Parameters& parameters) {
-  double coef = 0.;
-  if (SCHEME == CoefficientDiscretization::Implicit) {
-    DiffusionCoefficient<1, DIFFU_NAME> diff_coeff(u, parameters);
-    coef = diff_coeff.Eval(Tr, ip);
+  this->coeff_stab_ = params.get_param_value<double>("D");
+
+  for (const auto& p : params.get_vector()) {
+    const std::string& para_name = p.get_name();
+    const double& para_value = std::get<double>(p.get_value());
+    this->inter_diffusion_coeff_.emplace_back(para_name, para_value);
   }
-  return coef;
 }
 
 /**
@@ -138,27 +93,9 @@ double ThermoDiffusionNLFormIntegrator<SCHEME, DIFFU_NAME>::diffusion_prime(
 template <CoefficientDiscretization SCHEME, Diffusion DIFFU_NAME>
 ThermoDiffusionNLFormIntegrator<SCHEME, DIFFU_NAME>::ThermoDiffusionNLFormIntegrator(
     const mfem::ParGridFunction& u_old, const Parameters& params,
-    const std::vector<mfem::ParGridFunction>& aux_gf)
-    : u_old_(u_old), diffusion_params_(params), aux_gf_(aux_gf), mu_(u_old) {
-  // this->get_parameters(params);
-  this->coeff_stab = params.get_param_value<double>("D_stab");
-}
-
-/**
- * @brief Residual part of the non linear problem
- *
- * @param el
- * @param Tr
- * @param elfun
- * @param elvect
- */
-
-template <CoefficientDiscretization SCHEME, Diffusion DIFFU_NAME>
-void ThermoDiffusionNLFormIntegrator<SCHEME, DIFFU_NAME>::computeMu() {
-  for (int i = 0; i < this->u_old_.Size(); i++) {
-    this->mu_[i] = std::log(std::max(this->u_old_[i], 1e-10)) + 1;
-    // this->mu_[i] = this->u_old_[i];
-  }
+    const std::vector<mfem::ParGridFunction>& mu_gf)
+    : u_old_(u_old), mu_gf_(mu_gf) {
+  this->get_parameters(params);
 }
 
 /**
@@ -178,17 +115,24 @@ void ThermoDiffusionNLFormIntegrator<SCHEME, DIFFU_NAME>::AssembleElementVector(
   int dim = el.GetDim();
   int nElement = Tr.ElementNo;
 
-  this->gradPsi.SetSize(nd, dim);
   this->Psi.SetSize(nd);
-  this->gradPsiOld.SetSize(nd, dim);
-  this->gradU.SetSize(dim);
-  this->gradUOld.SetSize(dim);
+  this->gradPsi.SetSize(nd, dim);
+
+  this->gradMu_.SetSize(dim);
+
   elvect.SetSize(nd);
-  this->gradmu.SetSize(dim);
+  mfem::Vector grad_uold;
+  mfem::Vector grad_mu_i;
+  grad_mu_i.SetSize(dim);
+  grad_uold.SetSize(dim);
+
+  // Initialization
+  elvect = 0.0;
+
   const mfem::IntegrationRule* ir =
       &mfem::IntRules.Get(el.GetGeomType(), 2 * el.GetOrder() + Tr.OrderW());
-  elvect = 0.0;
-  this->computeMu();
+
+  // Loop over integration points
   for (int i = 0; i < ir->GetNPoints(); i++) {
     const mfem::IntegrationPoint& ip = ir->IntPoint(i);
 
@@ -197,32 +141,25 @@ void ThermoDiffusionNLFormIntegrator<SCHEME, DIFFU_NAME>::AssembleElementVector(
 
     const auto& u = elfun * Psi;
 
-    el.CalcPhysDShape(Tr, this->gradPsi);  // Compute derivative of shape function
-    this->gradPsi.MultTranspose(elfun, this->gradU);
+    // Stabilization contribution : D_stab * (Grad u - Grad un)
+    el.CalcPhysDShape(Tr, this->gradPsi);
+    this->gradPsi.MultTranspose(elfun, this->gradMu_);
+    this->u_old_.GetGradient(Tr, this->gradPsi, grad_uold);
 
-    this->u_old_.GetGradient(Tr, this->gradPsi, this->gradUOld);
-    this->mu_.GetGradient(Tr, this->gradPsi, this->gradmu);
+    this->gradMu_ -= grad_uold;
+    this->gradMu_ *= this->coeff_stab_;
 
-    /*
-      Two methods for calculating a gradient:
-      1/ this->mu_.GetGradient(Tr,this->gradmu);
-        simpler but very expensive
+    // Sum of [M_i * Grad mu_i] : explicit contribution (not present in Jacobian)
+    for (size_t i = 0; i < this->mu_gf_.size(); i++) {
+      auto mu_i = SlothGridFunction(this->mu_gf_.at(i));
+      mu_i.GetGradient(Tr, this->gradPsi, grad_mu_i);
+      const auto& M_i = std::get<1>(this->inter_diffusion_coeff_[i]);
+      grad_mu_i *= M_i;
+      this->gradMu_ += grad_mu_i;
+    }
+    this->gradMu_ *= ip.weight * Tr.Weight();
 
-      2/ this->mu.GetElementDofValues(nElement, this->elfunmu);
-         this->gradPsi.MultTranspose(this->elfunmu, this->gradmu);
-         Much faster execution (4 to 5 x faster)
-    */
-
-    const double coeff_diffu =
-        this->diffusion(Tr, ip, u, this->diffusion_params_) * ip.weight * Tr.Weight();
-
-    this->gradU *= this->coeff_stab * ip.weight * Tr.Weight();
-    this->gradUOld *= this->coeff_stab * ip.weight * Tr.Weight();
-    this->gradmu *= coeff_diffu;
-
-    this->gradPsi.AddMult(this->gradU, elvect, 1.0);
-    this->gradPsi.AddMult(this->gradUOld, elvect, -1.0);
-    this->gradPsi.AddMult(this->gradmu, elvect, 1.0);
+    this->gradPsi.AddMult(this->gradMu_, elvect, 1.0);
   }
 }
 
@@ -258,15 +195,9 @@ void ThermoDiffusionNLFormIntegrator<SCHEME, DIFFU_NAME>::AssembleElementGrad(
     const auto& u = elfun * Psi;
 
     Tr.SetIntPoint(&ip);
-    const double coeff_diffu =
-        this->diffusion(Tr, ip, u, this->diffusion_params_) * ip.weight * Tr.Weight();
+    const double coeff_diffu = this->coeff_stab_ * ip.weight * Tr.Weight();
     el.CalcPhysDShape(Tr, gradPsi);
     AddMult_a_AAt(coeff_diffu, gradPsi, elmat);
-
-    gradPsi.MultTranspose(elfun, gradU);
-    gradPsi.AddMult(gradU, vec);
-    const auto coef_diffu_derivative = this->diffusion_prime(Tr, ip, u, this->diffusion_params_);
-    AddMult_a_VWt(coef_diffu_derivative * ip.weight * Tr.Weight(), Psi, vec, elmat);
   }
 }
 
