3e960481390235c24a7fd97e25b20835fa171a95
INTROÏNI Clément ci230846 <clement.introini@cea.fr>
2025-01-16 13:30:08 +0100

    test: add a test to cover the use of the stabilization method dedicted to thermal diffusion problems

--- tests/Diffusion/1D/test3/description.md
+++ tests/Diffusion/1D/test3/description.md
@@ -1,4 +1,24 @@
-# 1D simulation (temporary)
+# 1D simulation 
+
+This example code aims at checking the implementation of a pseudo-stabilization method that can be used by thermal diffusion problems given by:
+$$
+\frac{\partial c}{\partial t}=\nabla \cdot{} (M \nabla \mu)\text{ in }\Omega 
+$$
+where $\mu$ is a chemical potential. 
+
+The method consists in adding a diffusive contribution in the RHS of the equation:
+$$
+\frac{\partial c}{\partial t}=\nabla \cdot{} (D \nabla c - D \nabla c) + \nabla \cdot{} (M \nabla \mu)\text{ in }\Omega 
+$$
+This contribution is obviously zero. However, its time discretization is given by:
+$$
+\nabla \cdot{} (M \nabla c^{n+1} - M \nabla c^{n})
+$$ 
+This test aims at checking that this contribution doesn't affect the solution driven by the chemical potential gradient. In other words, this test aims at doing a convergence analysis by showing that the solution of the following equation well converges towards the initial solution.
+
+$$
+\frac{\partial c}{\partial t}=\nabla \cdot{} (D \nabla c - D \nabla c) \text{ in }\Omega 
+$$
 
 
 ## Statement of the problem
@@ -7,9 +27,9 @@
 ### __Governing equation__
 Let us consider the following set of governing equations:
 $$
-\frac{\partial c}{\partial t}=[\nabla \cdot{} (M \nabla c - M \nabla c)]\text{ in }\Omega 
+\frac{\partial c}{\partial t}=\nabla \cdot{} (M \nabla c - M \nabla c)\text{ in }\Omega 
 $$
-
+The equation is solved in a 1D domain $\Omega=[0,1.e-3]$ (inline mesh).
 
 
 ### __Boundary conditions__
@@ -23,16 +43,16 @@ $$
 ### __Parameters__
 For this test, the following parameters are considered:
 
-| Parameter                          | Symbol     | Value                       |
-| ---------------------------------- | ---------- | --------------------------- |
-| Primary coefficient                | $M$        | $1.e-8$                       |
-| Domain size                        | $L$        | $1.e-3$                     |
+| Parameter           | Symbol | Value   |
+|---------------------|--------|---------|
+| Primary coefficient | $M$    | $1.e-8$ |
+| Domain size         | $L$    | $1.e-3$ |
 
 
 ### __Numerical scheme__
 
-- Time marching: Euler Implicit scheme, $t\in[0,0.5]$, $\delta t=0.1$
-- Spatial discretization: built from MFEM + order 1 FE 
+- Time marching: Euler Implicit scheme, $t\in[0,0.5]$, $\delta t=0.1$  
+- Spatial discretization: uniform mesh built from MFEM with $20$, $40$, $80$, $160$, $320$ elements of order 1 and 2
 
 
  
@@ -58,9 +78,9 @@ ctest -R Diffusion1Dtest3
 make Diffusion1Dtest3_coverage
 ```
 
-## Files & Dependencies
+## Expected results
 
-Source file : `main.cpp`
+![Results of the convergence study](ref/convergence_output.png)
 
 ## References
 
--- tests/Diffusion/1D/test3/main.cpp
+++ tests/Diffusion/1D/test3/main.cpp
@@ -29,8 +29,6 @@ int main(int argc, char* argv[]) {
   //---------------------------------------
 
   mfem::Mpi::Init(argc, argv);
-  int size = mfem::Mpi::WorldSize();
-  int rank = mfem::Mpi::WorldRank();
   mfem::Hypre::Init();
   //
   //---------------------------------------
@@ -39,7 +37,6 @@ int main(int argc, char* argv[]) {
   //---------------------------------------
   const auto DIM = 1;
   using NLFI =
-      // ThermoDiffusionNLFormIntegrator<CoefficientDiscretization::Implicit, Diffusion::Constant>;
       ThermoDiffusionNLFormIntegrator<CoefficientDiscretization::Explicit, Diffusion::Constant>;
   using FECollection = mfem::H1_FECollection;
   using PSTCollection = mfem::ParaViewDataCollection;
@@ -57,105 +54,112 @@ int main(int argc, char* argv[]) {
   // ##############################
   auto refinement_level = 0;
   double L = 1e-3;
-  int NN = 50;
-  SpatialDiscretization<FECollection, DIM> spatial("InlineLineWithSegments", 1, refinement_level,
-                                                   std::make_tuple(NN, L));
-  // ##############################
-  //     Boundary conditions     //
-  // // ##############################
-  auto boundaries = {Boundary("left", 0, "Neumann", 0.), Boundary("right", 1, "Neumann", 0.)};
-  auto bcs = BoundaryConditions<FECollection, DIM>(&spatial, boundaries);
+  std::vector<int> vect_order{1, 2};
+  std::vector<int> vect_NN{20, 40, 80, 160, 320};
+  for (const auto& order : vect_order) {
+    for (const auto& NN : vect_NN) {
+      SpatialDiscretization<FECollection, DIM> spatial("InlineLineWithSegments", order,
+                                                       refinement_level, std::make_tuple(NN, L));
+      // ##############################
+      //     Boundary conditions     //
+      // // ##############################
+      auto boundaries = {Boundary("left", 0, "Neumann", 0.), Boundary("right", 1, "Neumann", 0.)};
+      auto bcs = BoundaryConditions<FECollection, DIM>(&spatial, boundaries);
 
-  // ###########################################
-  // ###########################################
-  //            Physical models               //
-  // ###########################################
-  // ###########################################
-  // ####################
-  //     parameters    //
-  // ####################
-  const auto& diffusionCoeff(1e-8);
-  // const auto& diffusionCoeff(0.);
-  //  ####################
-  //      variables     //
-  //  ####################
+      // ###########################################
+      // ###########################################
+      //            Physical models               //
+      // ###########################################
+      // ###########################################
+      // ####################
+      //     parameters    //
+      // ####################
+      const auto& diffusionCoeff(1e-8);
+      // const auto& diffusionCoeff(0.);
+      //  ####################
+      //      variables     //
+      //  ####################
 
-  auto user_func =
-      std::function<double(const mfem::Vector&, double)>([L](const mfem::Vector& x, double time) {
-        const auto xx = x[0];
-        const auto epsilon = 1e-4;
-        auto func = (0.5 + 0.3 * std::tanh((xx - L / 2) / epsilon));
+      auto user_func = std::function<double(const mfem::Vector&, double)>(
+          [L](const mfem::Vector& x, double time) {
+            const auto xx = x[0];
+            const auto epsilon = 1e-4;
+            auto func = (0.5 + 0.3 * std::tanh((xx - L / 2) / epsilon));
 
-        const auto noiseLevel = 0.;
-        static std::random_device rd;
-        static std::mt19937 gen(rd());
-        std::normal_distribution<> d(0, noiseLevel);
+            const auto noiseLevel = 0.;
+            static std::random_device rd;
+            static std::mt19937 gen(rd());
+            std::normal_distribution<> d(0, noiseLevel);
 
-        return func + d(gen);
-      });
+            return func + d(gen);
+          });
 
-  auto user_func_analytical = std::function<double(const mfem::Vector&, double)>(
-      [L, diffusionCoeff](const mfem::Vector& x, double time) {
-        const auto xx = x[0];
-        const auto epsilon = 1e-3;
-        // auto func = 0.5 * (1 + std::tanh((xx - L / 2) / epsilon));
-        auto func = 0.5 * (1 + std::erf((xx - L / 2) / std::sqrt(4 * diffusionCoeff * time)));
+      auto user_func_analytical = std::function<double(const mfem::Vector&, double)>(
+          [L, diffusionCoeff](const mfem::Vector& x, double time) {
+            const auto xx = x[0];
+            const auto epsilon = 1e-3;
+            // auto func = 0.5 * (1 + std::tanh((xx - L / 2) / epsilon));
+            auto func = 0.5 * (1 + std::erf((xx - L / 2) / std::sqrt(4 * diffusionCoeff * time)));
 
-        return func;
-      });
+            return func;
+          });
 
-  auto initial_condition = AnalyticalFunctions<DIM>(user_func);
-  auto analytical_solution = AnalyticalFunctions<DIM>(user_func_analytical);
+      auto initial_condition = AnalyticalFunctions<DIM>(user_func);
+      auto analytical_solution = AnalyticalFunctions<DIM>(user_func_analytical);
 
-  auto vars = VAR(Variable<FECollection, DIM>(&spatial, bcs, "c", 2, initial_condition));
+      auto vars = VAR(
+          Variable<FECollection, DIM>(&spatial, bcs, "c", 2, initial_condition, initial_condition));
 
-  // ###########################################
-  // ###########################################
-  //      Post-processing                     //
-  // ###########################################
-  // ###########################################
-  const std::string& main_folder_path = "Saves_Nx_" + std::to_string(NN);
-  const auto& level_of_detail = 1;
-  const auto& frequency = 1;
-  std::string calculation_path = "Problem1";
-  auto p_pst =
-      Parameters(Parameter("main_folder_path", main_folder_path),
-                 Parameter("calculation_path", calculation_path), Parameter("frequency", frequency),
-                 Parameter("level_of_detail", level_of_detail));
-  // ####################
-  //     operators     //
-  // ####################
+      // ###########################################
+      // ###########################################
+      //      Post-processing                     //
+      // ###########################################
+      // ###########################################
+      const std::string& main_folder_path =
+          "Saves_order_" + std::to_string(order) + "_Nx" + std::to_string(NN);
+      const auto& level_of_detail = 1;
+      const auto& frequency = 1;
+      std::string calculation_path = "Problem1";
+      auto p_pst = Parameters(Parameter("main_folder_path", main_folder_path),
+                              Parameter("calculation_path", calculation_path),
+                              Parameter("frequency", frequency),
+                              Parameter("level_of_detail", level_of_detail));
+      // ####################
+      //     operators     //
+      // ####################
 
-  // Problem 1:
-  const auto crit_cvg_1 = 1.e-12;
-  OPE oper(&spatial, TimeScheme::EulerImplicit);
-  oper.overload_diffusion(Parameters(Parameter("D", diffusionCoeff)));
+      // Problem 1:
+      const auto crit_cvg_1 = 1.e-12;
+      OPE oper(&spatial, TimeScheme::EulerImplicit);
+      oper.overload_diffusion(Parameters(Parameter("D", diffusionCoeff)));
 
-  PhysicalConvergence convergence(ConvergenceType::ABSOLUTE_MAX, crit_cvg_1);
-  auto pst = PST(&spatial, p_pst);
+      PhysicalConvergence convergence(ConvergenceType::ABSOLUTE_MAX, crit_cvg_1);
+      auto pst = PST(&spatial, p_pst);
 
-  PB problem1("Problem 1", oper, vars, pst, convergence);
+      PB problem1(oper, vars, pst, convergence);
 
-  // Coupling 1
-  auto cc = Coupling("coupling 1 ", problem1);
+      // Coupling 1
+      auto cc = Coupling("coupling 1 ", problem1);
 
-  // ###########################################
-  // ###########################################
-  //            Time-integration              //
-  // ###########################################
-  // ###########################################
-  const auto& t_initial = 0.0;
-  const auto& t_final = 0.2;
-  const auto& dt = 1e-4;
-  auto time_params = Parameters(Parameter("initial_time", t_initial),
-                                Parameter("final_time", t_final), Parameter("time_step", dt));
-  auto time = TimeDiscretization(time_params, cc);
+      // ###########################################
+      // ###########################################
+      //            Time-integration              //
+      // ###########################################
+      // ###########################################
+      const auto& t_initial = 0.0;
+      const auto& t_final = 0.2;
+      const auto& dt = 1e-4;
+      auto time_params = Parameters(Parameter("initial_time", t_initial),
+                                    Parameter("final_time", t_final), Parameter("time_step", dt));
+      auto time = TimeDiscretization(time_params, cc);
 
-  time.solve();
-  //---------------------------------------
-  // Profiling stop
-  //---------------------------------------
-  Profiling::getInstance().print();
+      time.solve();
+      //---------------------------------------
+      // Profiling stop
+      //---------------------------------------
+      Profiling::getInstance().print();
+    }
+  }
   //---------------------------------------
   // Finalize MPI
   //---------------------------------------
