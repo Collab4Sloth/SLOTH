9ea817e91a5894e29c39269575f7b0d3801c0002
Clément Introïni <32884037+CINTROINI@users.noreply.github.com>
2025-04-10 12:59:23 +0200

    test: add covering test for variable defined with an attribute

---
+++ tests/AllenCahn/2D/test10/main.cpp
@@ -0,0 +1,152 @@
+/**
+ * @file main.cpp
+ * @author ci230846 (clement.introini@cea.fr)
+ * @brief Allen-Cahn problem solved in a 2D pellet fragment with a constant enthalpy melting
+ * @version 0.1
+ * @date 2024-05-23
+ *
+ * @copyright Copyright (c) 2024
+ *
+ */
+
+#include <iostream>
+#include <map>
+#include <memory>
+#include <sstream>
+#include <string>
+
+#include "kernel/sloth.hpp"
+#include "mfem.hpp"  // NOLINT [no include the directory when naming mfem include file]
+#include "tests/tests.hpp"
+///---------------
+/// Main program
+///---------------
+int main(int argc, char* argv[]) {
+  //---------------------------------------
+  // Initialize MPI and HYPRE
+  //---------------------------------------
+
+  mfem::Mpi::Init(argc, argv);
+  mfem::Hypre::Init();
+  //
+  //---------------------------------------
+  // Profiling
+  Profiling::getInstance().enable();
+  //---------------------------------------
+  /////////////////////////
+  const int DIM = 2;
+  using FECollection = Test<DIM>::FECollection;
+  using VARS = Test<DIM>::VARS;
+  using VAR = Test<DIM>::VAR;
+  using PSTCollection = Test<DIM>::PSTCollection;
+  using PST = Test<DIM>::PST;
+  using SPA = Test<DIM>::SPA;
+  using BCS = Test<DIM>::BCS;
+  /////////////////////////
+
+  using NLFI = AllenCahnConstantMeltingNLFormIntegrator<
+      VARS, ThermodynamicsPotentialDiscretization::Implicit, ThermodynamicsPotentials::W,
+      Mobility::Constant, ThermodynamicsPotentials::H>;
+
+  using OPE = AllenCahnOperator<FECollection, DIM, NLFI>;
+  using PB = Problem<OPE, VARS, PST>;
+  // ###########################################
+  // ###########################################
+  //         Spatial Discretization           //
+  // ###########################################
+  // ###########################################
+  // ##############################
+  //           Meshing           //
+  // ##############################
+  auto refinement_level = 0;
+  SPA spatial("GMSH", 1, refinement_level, "pellet2Dinclusion.msh", false);
+
+  // ##############################
+  //     Boundary conditions     //
+  // ##############################
+  auto boundaries = {Boundary("lower", 0, "Neumann", 0.), Boundary("external", 2, "Neumann", 0.),
+                     Boundary("upper", 1, "Neumann", 0.)};
+  auto bcs = BCS(&spatial, boundaries);
+
+  // ###########################################
+  // ###########################################
+  //            Physical models               //
+  // ###########################################
+  // ###########################################
+  // ####################
+  //     parameters    //
+  // ####################
+  //    Melting factor
+  const auto& alpha(-7.e3);
+  // Interface thickness
+  const auto& epsilon(5.e-4);
+  // Interfacial energy
+  const auto& sigma(6.e-2);
+  // Two-phase mobility
+  const auto& mob(1.e-5);
+  const auto& lambda = 3. * sigma * epsilon / 2.;
+  const auto& omega = 12. * sigma / epsilon;
+  auto params = Parameters(Parameter("epsilon", epsilon), Parameter("epsilon", epsilon),
+                           Parameter("sigma", sigma), Parameter("lambda", lambda),
+                           Parameter("omega", omega), Parameter("melting_factor", alpha));
+  // ####################
+  //     variables     //
+  // ####################
+  auto vars = VARS(VAR(&spatial, bcs, "phi", 2, 1., {"cluster"}));
+
+  // ###########################################
+  // ###########################################
+  //      Post-processing                     //
+  // ###########################################
+  // ###########################################
+  const std::string& main_folder_path = "Saves";
+  const auto& level_of_detail = 1;
+  const auto& frequency = 1;
+  std::string calculation_path = "Problem1";
+  auto p_pst1 =
+      Parameters(Parameter("main_folder_path", main_folder_path),
+                 Parameter("calculation_path", calculation_path), Parameter("frequency", frequency),
+                 Parameter("level_of_detail", level_of_detail));
+  auto pst = PST(&spatial, p_pst1);
+
+  // ####################
+  //     operators     //
+  // ####################
+
+  // Problem 1:
+  const auto crit_cvg_1 = 1.e-12;
+  OPE oper(&spatial, params, TimeScheme::EulerImplicit);
+  oper.overload_mobility(Parameters(Parameter("mob", mob)));
+
+  PhysicalConvergence convergence(ConvergenceType::ABSOLUTE_MAX, crit_cvg_1);
+
+  PB problem1("AllenCahn", oper, vars, pst, convergence);
+
+  // Coupling 1
+  auto cc = Coupling("Default Coupling", problem1);
+
+  // ###########################################
+  // ###########################################
+  //            Time-integration              //
+  // ###########################################
+  // ###########################################
+  const auto& t_initial = 0.0;
+  const auto& t_final = 10.;
+  const auto& dt = 0.25;
+  auto time_params = Parameters(Parameter("initial_time", t_initial),
+                                Parameter("final_time", t_final), Parameter("time_step", dt));
+  auto time = TimeDiscretization(time_params, cc);
+
+  // time.get_tree();
+  time.solve();
+  //---------------------------------------
+  // Profiling stop
+  //---------------------------------------
+  Profiling::getInstance().print();
+  //---------------------------------------
+  // Finalize MPI
+  //---------------------------------------
+  MPI_Finalize();
+  //---------------------------------------
+  return 0;
+}
