ec086e2e8b6e1b38a5b44e3dce6631027fb0a6d6
Clément Introïni <32884037+CINTROINI@users.noreply.github.com>
2025-11-26 13:50:32 +0100

    Change the getIVariable method by the operator[]

--- kernel/Variables/Variables.hpp
+++ kernel/Variables/Variables.hpp
@@ -4,24 +4,24 @@
  * @brief Variables class used to build and manage variables
  * @version 0.1
  * @date 2025-09-05
- * 
+ *
  * Copyright CEA (C) 2025
- * 
+ *
  * This file is part of SLOTH.
- * 
+ *
  * SLOTH is free software: you can redistribute it and/or modify
  * it under the terms of the GNU Lesser General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
- * 
+ *
  * SLOTH is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU Lesser General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU Lesser General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
- * 
+ *
  */
 #include <any>
 #include <functional>
@@ -50,10 +50,10 @@ class Variables {
 
   Variables();
 
+  Variable<T, DIM>& operator[](size_t i);
   void add(Variable<T, DIM> var);
   std::vector<Variable<T, DIM>> getVariables() const;
   size_t get_variables_number() const;
-  Variable<T, DIM>& getIVariable(const int& i);
   Variable<T, DIM>& get_variable(const std::string& name);
   std::map<std::string, mfem::ParGridFunction> get_map_gridfunction() const;
   std::map<std::string, Variable<T, DIM>> get_map_variable() const;
@@ -112,13 +112,14 @@ size_t Variables<T, DIM>::get_variables_number() const {
 }
 
 /**
- * @brief get i-th variable
+ * @brief Return the i-th variable
  *
  * @param i
- * @return Variable&
+ * @return Variable<T, DIM>&
  */
 template <class T, int DIM>
-Variable<T, DIM>& Variables<T, DIM>::getIVariable(const int& i) {
+Variable<T, DIM>& Variables<T, DIM>::operator[](size_t i) {
+  if (i >= vect_variables_.size()) throw std::out_of_range("Index out of range");
   return this->vect_variables_[i];
 }
 
