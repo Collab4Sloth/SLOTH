ca77d89f115451659a8803e6cae7abc661f8e48c
CINTROINI <CINTROINI@users.noreply.github.com>
2026-03-27 14:52:59 +0100

    Improve detection of Python interpreter

--- cmake/modules/SYMPY4SLOTH.cmake
+++ cmake/modules/SYMPY4SLOTH.cmake
@@ -1,4 +1,8 @@
+set(Python3_FIND_STRATEGY LOCATION)
+set(Python3_FIND_VIRTUALENV FIRST)
+
 find_package(Python3 REQUIRED COMPONENTS Interpreter)
+
 function(check_python_module module result_var)
     execute_process(
         COMMAND ${Python3_EXECUTABLE} -c "import ${module}"
