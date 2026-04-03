ceec2faaa25e50f9a9d46391cd231bf5bdea47ab
rprat-pro <rp269144@pleiades1217.intra.cea.fr>
2026-04-03 15:10:09 +0200

    update spack sloth package with spack@1.1

--- spack_repo/packages/sloth/package.py
+++ spack_repo/packages/sloth/package.py
@@ -15,14 +15,20 @@ class Sloth(CMakePackage):
     homepage = "https://github.com/Collab4Sloth/SLOTH"
     url      = "https://github.com/Collab4Sloth/SLOTH.git"
 
-    version("master",  branch='master', preferred=True)
+    version("master", git='https://github.com/Collab4Sloth/SLOTH.git', branch='master', preferred=True)
     version("1.0.0",  tag='v1.0.0')
 
     variant('petsc'       , default=False  , description='Enable PETSc solvers, preconditioners, etc.')
+    variant('shared'       , default=False  , description='Enable Building MFEM with dynamic libraries.')
 
     depends_on('hypre+int64', when='+petsc')
     depends_on('petsc+int64', when='+petsc')
     depends_on('mfem@4.8.0:+mpi+suite-sparse+sundials+superlu-dist+miniapps')
     depends_on('mfem@4.8.0:+petsc', when='+petsc')
+    depends_on('mfem@4.8.0:+shared', when='+shared')
     depends_on('cmake', type='build')
 
+    def setup_build_environment(self, env):
+        hypre_prefix = self.spec['hypre'].prefix
+        env.set('HYPRE_DIR', hypre_prefix)
+        env.set('USER_INSTALL_PATH', self.prefix)
