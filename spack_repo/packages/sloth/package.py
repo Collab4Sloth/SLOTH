8218743e9fa0f341a38d2ce0a36d089ed0c2fc34
Clément Introïni <32884037+CINTROINI@users.noreply.github.com>
2026-04-10 07:56:47 +0200

    Update Spack-Repo to account for MFEM 4.9 + Hypre 3.1.0 and prepare release of v2.0.0

--- spack_repo/packages/sloth/package.py
+++ spack_repo/packages/sloth/package.py
@@ -15,8 +15,8 @@ class Sloth(CMakePackage):
     homepage = "https://github.com/Collab4Sloth/SLOTH"
     url      = "https://github.com/Collab4Sloth/SLOTH.git"
 
-    version("master", git='https://github.com/Collab4Sloth/SLOTH.git', branch='master', preferred=True)
-    version("1.0.0",  tag='v1.0.0')
+    version("2.0.0",  tag='v2.0.0', preferred=True)
+    version("master", git='https://github.com/Collab4Sloth/SLOTH.git', branch='master')
 
     variant('petsc'       , default=False  , description='Enable PETSc solvers, preconditioners, etc.')
     variant('shared'       , default=False  , description='Enable Building MFEM with dynamic libraries.')
@@ -24,11 +24,11 @@ class Sloth(CMakePackage):
     depends_on('hypre+int64', when='+petsc')
     depends_on('petsc+int64', when='+petsc')
     
-    depends_on('hypre@2.33.0 +shared')
-    depends_on('mfem@4.8.0 +mpi+suite-sparse +sundials +superlu-dist +miniapps')
+    depends_on('hypre@3.1.0 +shared')
+    depends_on('mfem@4.9.0 +mpi+suite-sparse +sundials +superlu-dist +miniapps')
 
-    depends_on('mfem@4.8.0:+petsc', when='+petsc')
-    depends_on('mfem@4.8.0:+shared', when='+shared')
+    depends_on('mfem@4.9.0:+petsc', when='+petsc')
+    depends_on('mfem@4.9.0:+shared', when='+shared')
     depends_on('cmake', type='build')
 
     def setup_build_environment(self, env):
