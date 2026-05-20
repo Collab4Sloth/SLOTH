75067a136a87699440d908039ac0efb6d83cd979
Clément Introïni <32884037+CINTROINI@users.noreply.github.com>
2026-05-20 17:45:43 +0200

    Add Portable compiler option equivalent to Release with -march=x86_64

--- cmake/modules/COMPILE4SLOTH.cmake
+++ cmake/modules/COMPILE4SLOTH.cmake
@@ -15,6 +15,8 @@ elseif(CMAKE_BUILD_TYPE STREQUAL "MinSizeRel")
     message("MinSizeRel build.")
 elseif(CMAKE_BUILD_TYPE STREQUAL "RelWithDebInfo")
     message("RelWithDebInfo build.")
+elseif(CMAKE_BUILD_TYPE STREQUAL "PortableRelease")
+    message("PortableRelease build.")
 else()
     message("Release build.")
 endif()
@@ -31,6 +33,9 @@ set(DEBUG_OPTIONS -g -fstack-protector -O0 -gdwarf-4 -pedantic ${WARNINGS})
 # Release
 set(RELEASE_OPTIONS -O3 -DNDEBUG -DNO_BOUND_CHECK -march=native -flto -fno-plt)
 
+# Portable Release
+set(PORTABLE_REL_OPTIONS -O3 -DNDEBUG -march=x86-64 -mtune=generic )
+
 # RelWithDebInfo
 set(RELEASEWITHDEBINFO_OPTIONS -O2 -g -march=native -fno-omit-frame-pointer)
 
@@ -45,22 +50,25 @@ set(COVERAGE_LINK_OPTIONS --coverage)
 
 # Function called at each build 
 function(set_compile_options CURRENT_EXE)
-    if(CMAKE_BUILD_TYPE MATCHES Debug)
+    if(CMAKE_BUILD_TYPE STREQUAL "Debug")
         target_compile_options(${CURRENT_EXE} PRIVATE ${DEBUG_OPTIONS})
 
-    elseif(CMAKE_BUILD_TYPE MATCHES Optim)
+    elseif(CMAKE_BUILD_TYPE STREQUAL "Optim")
         target_compile_options(${CURRENT_EXE} PRIVATE ${OPTIM_OPTIONS})
 
-    elseif(CMAKE_BUILD_TYPE MATCHES Release)
+    elseif(CMAKE_BUILD_TYPE STREQUAL "Release")
         target_compile_options(${CURRENT_EXE} PRIVATE ${RELEASE_OPTIONS})
 
-    elseif(CMAKE_BUILD_TYPE MATCHES RelWithDebInfo)
+    elseif(CMAKE_BUILD_TYPE STREQUAL "PortableRelease")
+        target_compile_options(${CURRENT_EXE} PRIVATE ${PORTABLE_REL_OPTIONS})
+
+    elseif(CMAKE_BUILD_TYPE STREQUAL "RelWithDebInfo")
         target_compile_options(${CURRENT_EXE} PRIVATE ${RELEASEWITHDEBINFO_OPTIONS})
 
-    elseif(CMAKE_BUILD_TYPE MATCHES MinSizeRel)
+    elseif(CMAKE_BUILD_TYPE STREQUAL "MinSizeRel")
         target_compile_options(${CURRENT_EXE} PRIVATE ${MINSIZEREL_OPTIONS})
 
-    elseif(CMAKE_BUILD_TYPE MATCHES Coverage)
+    elseif(CMAKE_BUILD_TYPE STREQUAL "Coverage")
         target_compile_options(${CURRENT_EXE} PRIVATE ${COVERAGE_OPTIONS})
         link_libraries(gcov)
         set_target_properties(${CURRENT_EXE} PROPERTIES LINK_FLAGS ${COVERAGE_LINK_OPTIONS})
