58d4b378bb9642d5ba7646ef223817b46f00d006
Clément Introïni <32884037+CINTROINI@users.noreply.github.com>
2026-06-16 15:35:41 +0200

    Update ListIntegrators.hpp

--- kernel/include/Integrators/ListIntegrators.hpp
+++ kernel/include/Integrators/ListIntegrators.hpp
@@ -26,7 +26,6 @@
 
 #pragma once
 
-#include "Integrators/AllenCahnDiffusionControlNLFormIntegrator.hpp"
 #include "Integrators/AllenCahnNLFormIntegrator.hpp"
 #include "Integrators/BlockAllenCahnNLFormIntegrator.hpp"
 #include "Integrators/CahnHilliardNLFormIntegrator.hpp"
